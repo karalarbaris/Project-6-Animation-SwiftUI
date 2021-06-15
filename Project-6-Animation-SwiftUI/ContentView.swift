91cae6bfd2026217898d7319465ea92418409845
Baris <karalarbaris@gmail.com>
2021-06-15 20:33:24 +0200

    Initial Commit

---
+++ Project-6-Animation-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-6-Animation-SwiftUI
+//
+//  Created by Baris Karalar on 15.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-6-Animation-SwiftUI/Project_6_Animation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_6_Animation_SwiftUIApp.swift
+//  Project-6-Animation-SwiftUI
+//
+//  Created by Baris Karalar on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_6_Animation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
