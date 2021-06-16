05ea7f5cee27e7f2ec527fd50a9c212e494ee395
Baris <karalarbaris@gmail.com>
2021-06-16 14:51:31 +0200

    Controlling the animation stack

--- Project-6-Animation-SwiftUI/ContentView.swift
+++ Project-6-Animation-SwiftUI/ContentView.swift
@@ -7,32 +7,53 @@
 
 import SwiftUI
 
+//Controlling the animation stack
+
 struct ContentView: View {
     
-    @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         
         Button("Tap Me") {
-//            withAnimation {
-//                animationAmount += 360
-//            }
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                animationAmount += 360
-            }
+            enabled.toggle()
         }
-        .padding(50)
-        .background(Color.red)
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(nil)
         .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(
-            .degrees(animationAmount),
-            axis: (x: 0.0, y: 1.0, z: 0.0)
-        )
-        
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
         
     }
 }
+
+//struct ContentView: View {
+//
+//    @State private var animationAmount = 0.0
+//
+//    var body: some View {
+//
+//        Button("Tap Me") {
+////            withAnimation {
+////                animationAmount += 360
+////            }
+//            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+//                animationAmount += 360
+//            }
+//        }
+//        .padding(50)
+//        .background(Color.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .rotation3DEffect(
+//            .degrees(animationAmount),
+//            axis: (x: 0.0, y: 1.0, z: 0.0)
+//        )
+//
+//
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
